9541277d92b1c75ef2bfdde4544b2c6bd8cd133c
fissehab <fishjhu@gmail.com>
2015-05-15 20:36:23 -0400

    completed best hospital by state

--- rankall.R
+++ rankall.R
@@ -1,5 +1,5 @@
 
-setwd("C:/Fish/classes/spring_2015/R/programming_assignments/3")
+# setwd("C:/Fish/classes/spring_2015/R/programming_assignments/3")
 
 rankall <- function(outcome,num='best') {
     
@@ -30,42 +30,54 @@ rankall <- function(outcome,num='best') {
          a<-data[,c(2,7,23)]
      }}
     
+    
     a[,3]<-as.numeric(a[,3])
+    
     
     colnames(a)<-cbind('hospital','state','value')
     
-    b<- split(a,a$state)
+    states<-sort(unique(a$state))
     
-    c<-names(b)
-     ans<-list()
-    for  (letter in c){
-        d<-b[[letter]]
-         if (num=='best'){
-                              
-           e<- d[which(d[,3]==min(d[,3],na.rm=TRUE)),]
+    b<-split(a,a$state)
+    
+    ans_state<-c()
+    ans_hospital<-c()
+    
+    
+    
+    
+    for (letter in states){
 
-           hospitals<-e$hospital
-           hospital<-sort(hospitals)[1]
-           ans<-rbind(ans,e[hospital,])
-     
-     }
-     
-     if (num=='worst'){
-         
-         d<- c[which(c[,2]==max(c[,2],na.rm=TRUE)),]
-         
-         hospitals<-d$Hospital.Name
-         hospital<-sort(hospitals)[1]
-         
-     }
-     
-    if(is.numeric(num) & num < length(c$Hospital.Name)){
+   
+    g<-b[[letter]]
+    
+     g<-g[,c(1,3)]
+
+    if (num=='best'){
+        
+        d<- c[which(g[,2]==min(g[,2],na.rm=TRUE)),]
+        
+        hospitals<-d$hospital
+        hospital<-sort(hospitals)[1]
+        
+    }
+    
+    if (num=='worst'){
+        
+        d<- g[which(g[,2]==max(g[,2],na.rm=TRUE)),]
+        
+        hospitals<-d$hospital
+        hospital<-sort(hospitals)[1]
+        
+    }
+    
+    if(is.numeric(num) & num < length(g$hospital)){
         
         
-        l<-c[!is.na(c[,2]),]
+        l<-g[!is.na(g[,2]),]
         ll<-unique(sort(l[,2]))
         
-         m<-c()
+        m<-c()
         
         for ( i in 1:length(ll)){
             temp<-which(l[,2]==ll[i])
@@ -80,20 +92,22 @@ rankall <- function(outcome,num='best') {
         
         hospitals<-sort(m[p,1])
         
-        hospital<-hospitals[num2] 
-        
-    }
-     
-    if (is.numeric(num)){
-        if (num > length(c$Hospital.Name)){ 
-            return(NA)}}
-
+        hospital<-hospitals[num2]
     }
     
-    hospital   
-}
-
-
-
-
+    if (is.numeric(num)){
+        if (num > length(g$hospital)){ 
+            hospital<-NA}}
+    
+    ans_state<-c(ans_state,letter)
+    ans_hospital<-c(ans_hospital,hospital)
+    
+    }
+    
+    df = data.frame(ans_hospital, ans_state)       # df is a data frame 
+    colnames(df)<-c('hospital','state')
+    
+    df
+} 
+    
 
